a56449e0ec0bb0010c56d902b0c2afb4d6cfeb1e
Verustus <kukuleluk@gmail.com>
2023-10-12 12:45:27 +0200

    .

---
+++ projects/10.10.23/info.js
@@ -0,0 +1,20 @@
+var data = [
+    {
+        "id": 1,
+        "name": "Pepa",
+        "surname": "Mít",
+        "email": "pepamit@gmail.com",
+        "phone": "602146514",
+        "birth": "12.24.23",
+        "sex": "M"
+    },
+    {
+        "id": 1,
+        "name": "Pepa",
+        "surname": "Mít",
+        "email": "pepamit@gmail.com",
+        "phone": "602146514",
+        "birth": "12.24.23",
+        "sex": "M"
+    }
+]
---
+++ projects/10.10.23/script.js
@@ -0,0 +1,16 @@
+var table = document.getElementById("data");
+for(var i = 0; i < data.length; i++) {
+    var tr = document.createElement("tr");
+    addDataToTable(tr, data[i].name);
+    addDataToTable(tr, data[i].surname);
+    addDataToTable(tr, data[i].email);
+    addDataToTable(tr, data[i].phone);
+    addDataToTable(tr, data[i].birth);
+    addDataToTable(tr, data[i].sex);
+}
+
+function addDataToTable(parent, text) {
+    var element = document.createElement("td");
+    element.innerText = text;
+    parent.appendChild(element);
+}
