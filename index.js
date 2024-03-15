4712aa096def46c4442cb70cb1b216360025d6e7
Verustus <kukuleluk@gmail.com>
2024-03-15 11:30:31 +0100

    show date hide date

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const withDate = true;
+let withDate = true;
 const user = "verustus";
 const repo = "SPSUL-WEA";
 const defaultDirectory = "projects";
 
 const url = `https://api.github.com/repos/${user}/${repo}/git/trees/master?recursive=1`;
 const urlParams = new URLSearchParams(window.location.search);
-var currentDirectory = defaultDirectory;
+let currentDirectory = defaultDirectory;
 
-var data;
-update();
+start();
+
+async function start() {
+    if (urlParams.get("showDate") != null) withDate = urlParams.get("showDate") == "true";
+    updateDate(withDate);
+}
+
+function updateDate(to) {
+    if (to != undefined) withDate = to;
+    else withDate = !withDate;
+    setParam("showDate", withDate);
+    const showDateButton = document.getElementById("showDate");
+    if (withDate) showDateButton.innerText = "Hide date";
+    else showDateButton.innerText = "Show date";
+    update(currentDirectory);
+}
 
 async function loadData() {
     function onError(error) {
@@ -23,17 +37,26 @@ async function loadData() {
     } catch (error) { onError(error.message); }
 }
 
+function setParam(param, value) {
+    urlParams.set(param, value);
+    let params = "";
+    urlParams.forEach((val, key) => {
+        if (key == param) val = value;
+        params += `${key}=${val}&`;
+    });
+    window.history.replaceState(null, null, `${window.location.pathname}?${urlParams}`);
+}
+
 async function update(url) {
-    if (url) {
-        urlParams.set("directory", url);
-        window.history.replaceState(null, null, `${window.location.pathname}?directory=${url}`);
-    }
+    if (url) setParam("directory", url);
     if (urlParams.get("directory") != null && urlParams.get("directory") != currentDirectory) {
         currentDirectory = "" + removeWrongSlash(urlParams.get("directory"));
         if (currentDirectory != defaultDirectory) changeStatus("./" + currentDirectory);
         else changeStatus("");
     }
     clearList();
+
+    let data;
     if (!data) data = await loadData();
     if (data) setAllContent(data, currentDirectory);
 }
@@ -42,7 +65,7 @@ async function setAllContent(data, pathToDirectory) {
     const parentList = JSON.parse(JSON.stringify(data));
     function isBetween(i, from, to) { return i >= from && i <= to; }
     function getDateFromName(name) {
-        var strDate = name.split(".");
+        let strDate = name.split(".");
         strDate.slice(0, 3);
         if (strDate.length == 3) {
             strDate.forEach((item, index) => strDate[index] = item.replace(/[^\d.]/g, ''));
@@ -50,19 +73,24 @@ async function setAllContent(data, pathToDirectory) {
             strDate.forEach((item, index) => strDate[index] = parseInt(item));
         }
         else strDate.forEach((item, index) => strDate[index] = undefined);
-        return (isBetween(strDate[0], 1, 12) && isBetween(strDate[1], 1, 31) && isBetween(strDate[2], 0, 100)) ? new Date(strDate[2]+2000, strDate[0]-1, strDate[1]) : undefined;
+        return (isBetween(strDate[0], 1, 12) && isBetween(strDate[1], 1, 31) &&
+                isBetween(strDate[2], 0, 100)) ?
+                new Date(strDate[2]+2000, strDate[0]-1, strDate[1]) : undefined;
     }
     const fileList = [{ index: 1, url: defaultDirectory, displayname: "./"}];
     parentList.tree = parentList.tree.filter(file => file.path.startsWith(pathToDirectory));
-    parentList.tree.forEach((file, index) => parentList.tree[index].path = removeWrongSlash(file.path.substring(pathToDirectory.length)));
+    parentList.tree.forEach((file, index) => 
+        parentList.tree[index].path = removeWrongSlash(file.path.substring(pathToDirectory.length)));
     if (parentList.tree.find(file => file.path == "")) {
-        const directoryFiles = parentList.tree.filter(file => (file.type == "tree" && file.path.split("/").length == 1 && file.path != "") || (file.type == "blob" && (file.path.split("/").length == 1 || file.path.split("/")[1] == "index.html")));
+        const directoryFiles = parentList.tree.filter(file =>
+            (file.type == "tree" && file.path.split("/").length == 1 && file.path != "") ||
+            (file.type == "blob" && (file.path.split("/").length == 1 || file.path.split("/")[1] == "index.html")));
         directoryFiles.forEach(file => {
             if (file.path.split("/").length == 1) {
-                var redirect = true;
-                var url = pathToDirectory + "/" + file.path;
-                var displayname = withDate ? file.path : (file.path.split(" - ").length >= 2 ? file.path.substring(file.path.split(" - ")[0].length + 3) : file.path);
-                var date = getDateFromName(file.path);
+                let redirect = true;
+                let url = pathToDirectory + "/" + file.path;
+                let displayname = withDate ? file.path : (file.path.split(" - ").length >= 2 ? file.path.substring(file.path.split(" - ")[0].length + 3) : file.path);
+                let date = getDateFromName(file.path);
                 if (file.type === "tree") {
                     if (!directoryFiles.find(isIndex => isIndex.path == file.path + "/index.html")) {
                         url = `${pathToDirectory}/${file.path}`;
@@ -87,6 +115,7 @@ async function setAllContent(data, pathToDirectory) {
         if (b.date) return -1;
         return 0;
     });
+
     fileList.forEach((data) => {
         const onclick = data.redirect ? undefined : (data.url == "" ? defaultDirectory : data.url);
         addFileToList(data.redirect ? data.url : `?directory=${data.url}`, data.displayname, onclick);
@@ -111,7 +140,7 @@ function addFileToList(url, name, onclick) {
 function clearList() { document.getElementById("files").innerHTML = ""; }
 
 function changeStatus(str) {
-    var status = document.getElementById("status");
+    let status = document.getElementById("status");
     if (!str) status.style.display = "none";
     else {
         status.innerText = str;
