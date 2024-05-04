b99fa43a342c49867d7cbad9c572fba58f54f0c3
Semicolon Mystery <74712225+semicolonmystery@users.noreply.github.com>
2024-05-04 16:10:26 +0200

    .

--- README.md
+++
@@ -1,4 +0,0 @@
-# SPSUL-WEA
-Web projects that I have done on SPSUL school.
-
-Go to https://semicolonmystery.github.io/SPSUL-WEA/
--- projects/11.13.23 - about me and portfolio/contact.js
+++
@@ -1,7 +0,0 @@
-function sendEmail() {
-	const firstName = document.contactMeForm.firstName.value;
-	const lastName = document.contactMeForm.lastName.value;
-	const email = document.contactMeForm.email.value;
-	const password = document.contactMeForm.password.value;
-    console.log(firstName);
-}
